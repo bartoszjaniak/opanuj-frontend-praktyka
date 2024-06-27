db81b943e7de905276f1ede2d24a517a00581a1f
Bartosz Janiak <bartosz.janiak@ringieraxelspringer.pl>
2024-06-27 20:10:00 +0200

    logging time in interceptors

--- examples/module1/lesson4/axios-interceptor/index.js
+++ examples/module1/lesson4/axios-interceptor/index.js
@@ -1,18 +1,19 @@
 import axios from 'axios';
 
 // Add a request interceptor
-axios.interceptors.request.use(function (config) {
-  return config;
+axios.interceptors.request.use(function(config) {
+    console.time(config.url);
+    return config;
 });
 
 // Add a response interceptor
-axios.interceptors.response.use(function (response) {
-  // Do something with response data
-  return response;
+axios.interceptors.response.use(function(response) {
+    console.timeLog(response.config.url);
+    return response;
 });
 
 const {
-  data: { articles },
+    data: { articles },
 } = await axios.get('/api/data/articles?timeout=3000');
 
 document.querySelector('#data').innerHTML = articles[0].content;
